81b3db2d96ba5119d5f7a2a9301869992d864bf6
AayushSaini101 <kumaraayush9810@gmail.com>
2024-03-24 14:11:23 +0530

    sdf

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -1,35 +1,31 @@
 const fs = require('fs');
+
+// Path to your JSON file
 const filePath = '../../users.json';
-const commenterName = "AayushSaini101"
 
-let isTSCMember = false;
-function readFileAndProcess(callback) {
+// Read the JSON file
 fs.readFile(filePath, 'utf8', (err, data) => {
-try {
-  const jsonData = JSON.parse(data);
+    if (err) {
+        console.error('Error reading file:', err);
+        return;
+    }
 
-  // Iterate over each object in the array
-  jsonData.forEach(item => {
-      console.log(item.github);
-      if (item.github === commenterName) {
-          isTSCMember = true;
-      }
-  });
+    try {
+        // Parse JSON data
+        const jsonData = JSON.parse(data);
 
-  // Invoke the callback function with the result
-  callback(null, isTSCMember);
-} catch (parseError) {
-  callback(parseError, null);
-}
-});
-}
-
-
-readFileAndProcess((error, result) => {
-if (error) {
-  console.error('Error:', error);
-} else {
-  console.log('Is TSC Member:', result);
-   core.setOutput('isTSCMember', isTSCMember);
-}
+        // Iterate over each object in the array
+        jsonData.forEach(item => {
+            console.log('Name:', item.name);
+            console.log('GitHub:', item.github);
+            console.log('LinkedIn:', item.linkedin);
+            console.log('Slack:', item.slack);
+            console.log('Twitter:', item.twitter);
+            console.log('Available for hire:', item.availableForHire);
+            console.log('Repos:', item.repos.join(', '));
+            console.log(''); // Add an empty line for separation
+        });
+    } catch (parseError) {
+        console.error('Error parsing JSON data:', parseError);
+    }
 });
