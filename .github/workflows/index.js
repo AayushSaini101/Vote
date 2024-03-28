4efb8cb04d5fc31b7c9375f3430edb1ae3c69f0d
AayushSaini101 <kumaraayush9810@gmail.com>
2024-03-28 08:28:26 +0530

    Addsf

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -1,31 +1,18 @@
-const fs = require('fs').promises; // using promises version of fs
-const filePath = 'TSC_MEMBERS.json';
-const commenterName = '${{inputs.authorName}}';
-console.log(commenterName);
-let isTSCMember = false;
-
-async function readFileAndProcess() {
-  try {
-    const data = await fs.readFile(filePath, 'utf8');
-    const jsonData = JSON.parse(data);
-    // Iterate over each object in the array
-    jsonData.forEach(item => {
-      if (item.github === commenterName) {
-        isTSCMember = true;
-      }
-    });
-    return isTSCMember;
-  } catch (error) {
-    throw error;
-  }
-}
-
-(async () => {
-  try {
-    const result = await readFileAndProcess();
-    console.log('Is TSC Member:', result);
-    core.setOutput('isTSCMember', result);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-})();
+         const yaml = require('js-yaml');
+          const fs = require('fs');
+          const commenterName = 'AayushSaini101';
+          let isTSCMember = false;
+          try {
+              // Load YAML file
+              const data = yaml.load(fs.readFileSync('../../MAINTAINERS.yaml', 'utf8'));
+              // Iterate over each person object
+              data.forEach(person => {
+                  // Check if the person is a TSC member or not
+                  if (person.isTscMember && person.github == commenterName) {
+                      isTSCMember = true;
+                      core.setOutput('isTSCMember', result);
+                  }
+              });
+          } catch (e) {
+              console.log(e);
+          }
