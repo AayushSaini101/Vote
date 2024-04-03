2c889788fb50330fa817cdda0540d78a7a8b2513
AayushSaini101 <kumaraayush9810@gmail.com>
2024-04-03 23:14:01 +0530

    Checked

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -1,18 +1,24 @@
-         const yaml = require('js-yaml');
-          const fs = require('fs');
-          const commenterName = 'AayushSaini101';
-          let isTSCMember = false;
-          try {
-              // Load YAML file
-              const data = yaml.load(fs.readFileSync('../../MAINTAINERS.yaml', 'utf8'));
-              // Iterate over each person object
-              data.forEach(person => {
-                  // Check if the person is a TSC member or not
-                  if (person.isTscMember && person.github == commenterName) {
-                      isTSCMember = true;
-                      core.setOutput('isTSCMember', result);
-                  }
-              });
-          } catch (e) {
-              console.log(e);
-          }
+const yaml = require('js-yaml');
+const fs = require('fs');
+const commenterName = 'AayushSaini101';
+
+try {
+    // Load YAML file
+    const data = yaml.load(fs.readFileSync('../../MAINTAINERS.yaml', 'utf8'));
+
+    let isTSCMember = false;
+    
+    // Filter persons who are TSC members and whose GitHub username matches commenterName
+    const filteredPersons = data.filter(person => {
+        return (person.isTscMember === true || person.isTscMember === "true") && person.github === commenterName;
+    });
+
+    // Log filtered persons
+    filteredPersons.forEach(person => {
+        isTSCMember = true;
+    });
+   
+    core.setOutput('isTSCMember', isTSCMember);
+} catch (e) {
+    console.log(e);
+}
