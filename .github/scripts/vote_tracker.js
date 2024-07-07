bb17024af734ed3facae2b4741d1967c57ece37d
AayushSaini101 <kumaraayush9810@gmail.com>
2024-07-07 14:05:31 +0530

    Update the path

--- .github/scripts/vote_tracker.js
+++ .github/scripts/vote_tracker.js
@@ -10,7 +10,7 @@ module.exports = async ({context}) => {
   const orgName = context.issue.owner
   const repoName = context.issue.repo
 
-  const filePath = path.join('VoteTracking.json');
+  const filePath = path.join('voteTracking.json');
 
   const votingRows = parseVoteClosedComment()
   
